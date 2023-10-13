b2bdb8146ca22b763fc3828dd8fc1dc5d7a43592
Anju-kumari2021 <144138903+Anju-kumari2021@users.noreply.github.com>
2023-10-13 23:59:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const p = document.getElementById('count')
+let c= 0;
+function increment()
+  {
+c++;
+p.textContent = c;
+}
+
+
+
+const q = document.getElementById('count')
+let d= 21; 
+function decrement()
+  {
+d--;
+q.textContent = d;
+  }
+
+
+
+
+  const s = document.getElementById('count')
+let r= 0; 
+function reset()
+  {
+    r=0;
+s.textContent = r;
+  }
+
